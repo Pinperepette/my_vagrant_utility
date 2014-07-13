854d1db9ac62d016265b52199d56ba4444079488
Andrea Amani <Pinperepette@iMac-di-Andrea.local>
2014-07-13 14:03:18 +0200

    my Vagrant utility file

---
+++ pulisci.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Recupero un po di spazzio... XD
+umount /vagrant 
+apt-get clean -y
+apt-get autoclean -y 
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+find /var/lib/apt -type f | xargs rm -f
+find /var/lib/doc -type f | xargs rm -f 
+rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest* 
+rm -rf /usr/src/linux-headers* 
+find /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu} -type d -delete
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`;
+count=$((count -= 1))
+dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
+rm /tmp/whitespace;
+count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+count=$((count -= 1))
+dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
+rm /boot/whitespace;
+swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
+swapoff $swappart;
+dd if=/dev/zero of=$swappart;
+mkswap $swappart;
+swapon $swappart;
